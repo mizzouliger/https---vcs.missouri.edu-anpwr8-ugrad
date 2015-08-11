eeefa3c9069e5dbd24590f03b9cc4b1ab125941b
aditya <anpwr8@mail.missouri.edu>
2015-08-11 10:39:43 -0500

    adding base folder

---
+++ Footer.php
@@ -0,0 +1,9 @@
+
+
+</div>
+<div id="app-logo">
+	<img alt="title" src="resources/images/logo-biz.png">
+</div>
+<div id="footer">&copy; Copyright <?php echo date('Y'); ?> Curators of the University of Missouri</div>
+</body>
+</html>
---
+++ Header.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+
+<html class="no-js" lang="en">
+	<head id="apps-business-missouri-edu" data-template-set="html5-reset">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	    <meta charset="utf-8" />
+	    <meta name="title" content="Business Advising" />
+	    <meta name="description" content="Business Advising" />
+	    <meta name="Copyright" content="Copyright Curators of the University of Missouri. All Rights Reserved." />
+	    <meta name="DC.title" content="<?php echo APP_TITLE ;?>" />
+	    <title><?php echo Grad_APP ;?></title>
+		<link rel="shortcut icon" href="./resources/images/favicon.ico" />
+	    <!-- <link rel="stylesheet" type="text/css" href="resources/css/base.css"> -->
+	    <link rel="stylesheet" type="text/css" href="//apps.business.missouri.edu/css/base-1.1.css">
+	   <!-- change this file path to w.e we are going to be using -->
+         <link rel="stylesheet" type="text/css" href="resources/css/app.css">
+	    <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+	</head>
+	<body>
+	<div id="page">
+		<a href="./index.php" style="text-decoration: none;">
+		<div id="header">
+            <div id="app-title">
+             <?php echo 'Graduation Application';?>
+             <span><?php  echo "Trulaske College of Business" ;?></span> 
+            </div>
+        </div>
+       </a>
+      <div id="content">
